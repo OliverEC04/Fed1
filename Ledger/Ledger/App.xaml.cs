37f96437082465703aca6c0a0aad5a1f5506c0f4
OliverEC <oliverec04@gmail.com>
2024-03-05 10:38:07 +0100

    ledger

---
+++ Ledger/Ledger/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Ledger
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
