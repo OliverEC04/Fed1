a4d961d17dba473ddc6ff688ff8de557090ad4bf
OliverEC <oliverec04@gmail.com>
2024-03-05 10:46:54 +0100

    ledger

---
+++ Ledger/Ledger/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Ledger
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Ledger/Ledger/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Ledger
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
