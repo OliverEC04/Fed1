f3d6318930458f1bab2fd94028fe2d7e4262434e
Morten-T <114079510+Morten-T@users.noreply.github.com>
2024-03-06 11:58:22 +0100

    added debtorinfo viewmodel

---
+++ Ledger/Ledger/ViewModels/DebtorListViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ledger.ViewModels
+{
+    public partial class DebtorListViewModel : ObservableObject
+    {
+        [ObservableProperty] private List<Tuple<string, int>> ?_debtorDetails;
+
+        //public DebtorListPageViewModel()
+        //{
+        //    Title = "Hello, MVVM Toolkit!";
+        //}
+    }
+}
