ea4ea3dd0626a3316bd1b69872deb22ff1c8bb8a
OliverEC <oliverec04@gmail.com>
2024-03-06 12:40:14 +0100

    greg er homo

--- Ledger/Ledger/MauiProgram.cs
+++ Ledger/Ledger/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using CommunityToolkit.Maui;
 
 namespace Ledger
 {
@@ -9,6 +10,7 @@ namespace Ledger
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
