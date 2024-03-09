c675cef45eaab799a6cbc1bea0c4efda0f82c143
OliverEC <oliverec04@gmail.com>
2024-03-09 19:26:11 +0100

    frontpage

---
+++ Ledger/Ledger/Models/Debtor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ledger.Models
+{
+    public class Debtor
+    {
+        public string Name { get; set; }
+        public int Debt { get; set; }
+    }
+}
--- Ledger/Ledger/ViewModels/FrontPageViewModel.cs
+++ Ledger/Ledger/ViewModels/FrontPageViewModel.cs
@@ -1,28 +1,32 @@
 ﻿using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Ledger.Models;
 using Ledger.Pages;
 
 
 namespace Ledger.ViewModels
 {
-	public partial class FrontPageViewModel : ObservableObject
-	{
-		[ObservableProperty]
-		private string _title = "Debtors";
+    public partial class FrontPageViewModel : ObservableObject
+    {
         [ObservableProperty]
-        private ObservableCollection<Tuple<string, int>> _debtors;
+        private string _title = "Debtors";
+        [ObservableProperty]
+        private ObservableCollection<Debtor> _debtors;
 
-		[RelayCommand]
-		public async void AddDebtor()
-		{
-			await Application.Current.MainPage.Navigation.PushAsync(new AddDebtorPage()); // oh ja der kom jeg
-		}
+        [RelayCommand]
+        public async void AddDebtor()
+        {
+            await Application.Current.MainPage.Navigation.PushAsync(new AddDebtorPage()); // oh ja der kom jeg
+        }
 
+        public FrontPageViewModel()
+        {
+            Debtors = new()
+            {
+                new Debtor { Name="Testguy", Debt=100}
 
-		public FrontPageViewModel()
-		{
-
-		}
-	}
+            };
+        }
+    }
 }
