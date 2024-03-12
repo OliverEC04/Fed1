1195542813411b808aedc118bf3553da65c5ad67
OliverEC <oliverec04@gmail.com>
2024-03-12 12:33:04 +0100

    dark mode

--- Ledger/Ledger/App.xaml.cs
+++ Ledger/Ledger/App.xaml.cs
@@ -7,6 +7,8 @@ namespace Ledger
 		public App()
 		{
 			InitializeComponent();
+			if (Microsoft.Maui.Controls.Application.Current != null)
+				Microsoft.Maui.Controls.Application.Current.UserAppTheme = AppTheme.Dark;
 			MainPage = new NavigationPage(new FrontPage());
 		}
 	}
